a22c6c161eb4471aa59c27d1dbbf69b0346a2c07
Yeskendir Salgara <36730580+sorunokoe@users.noreply.github.com>
2024-06-13 17:11:34 +0200

    Clean code

--- Sources/AiComposableFeature/Views/CategoriesView.swift
+++ Sources/AiComposableFeature/Views/CategoriesView.swift
@@ -12,13 +12,6 @@ struct CategoriesView: View {
     var onDidSelect: (String) -> Void
 
     @State private var fieldValue: String = ""
-    @State private var dateValue = Date.now
-
-    private let dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        return formatter
-    }()
 
     init(
         items: [String],
@@ -53,7 +46,7 @@ struct CategoriesView: View {
                         }, label: {
                             RoundedRectangle(cornerRadius: 20)
                                 .fill(.white.opacity(0.1))
-                                .frame(height: 70)
+                                .frame(height: 65)
                                 .overlay {
                                     Text(city)
                                         .font(.system(size: 18, weight: .regular))
@@ -68,38 +61,6 @@ struct CategoriesView: View {
             }
         }
     }
-
-//    @ViewBuilder
-//    var categorySectionView: some View {
-//        if let selectedCategory {
-//            HStack(alignment: .center, spacing: 0) {
-//                ForEach(categories, id: \.self) { category in
-//                    Button(action: {
-//                        withAnimation {
-//                            selectedCategory = category
-//                        }
-//                    }, label: {
-//                        RoundedRectangle(cornerRadius: 20)
-//                            .fill(selectedCategory == category ? .white.opacity(0.1) : .clear)
-//                            .frame(height: 50)
-//                            .background {
-//                                Text(category.title)
-//                                    .font(.system(size: 20, weight: .medium))
-//                                    .frame(maxWidth: .infinity, alignment: .center)
-//                            }
-//                            .opacity(selectedCategory == category ? 1 : 0.5)
-//                    })
-//                    .foregroundStyle(.white)
-//                }
-//            }
-//            .background(.white.opacity(0.1))
-//            .clipShape(RoundedRectangle(cornerRadius: 20))
-//            .padding(.horizontal, 20)
-//            .shadow(radius: 5)
-//        } else {
-//            EmptyView()
-//        }
-//    }
 }
 
 #Preview {
